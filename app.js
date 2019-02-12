f8b83b4daadc7368383ed8cdfaf9466d495fb05f
MitchellTroyer <mtroyer95@att.net>
2019-02-12 12:20:32 -0500

    .env

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
-app.listen(3005, function(){
-    console.log('App is listening on 3005.')
-});
+app.listen(process.env.PORT, function(){});
