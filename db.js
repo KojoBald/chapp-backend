7fcee447ac4b46d60c12901bab4840009da7fc1f
Kaleb Baldwin <baldwinjkaleb@gmail.com>
2019-02-15 11:14:42 -0500

    connect to heroku postgres database and add dev script to run locally

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
-const Sequelize = require('sequelize'); 
+const sequelize = require('sequelize-heroku').connect(require('sequelize'));
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, 'postgres', process.env.DATABASE_PASS, {
-    host: process.env.DATABASE_HOST,
-    dialect: 'postgres'
-})
- 
-sequelize.authenticate().then(
-    function() {
-        console.log('connected')
-    },
-    function(err) {
-        console.log(err)
-    }
-)
+if(sequelize) {
+    sequelize.authenticate()
+        .then(() => {
+            let config = sequelize.connectionManager.config;
+            console.log(`sequelize-heroku: connected to ${config.host} as ${config.username}`)
+        }).catch(error => {
+            let config = sequelize.connectionManager.config;
+            console.error(`sequelize-heroku: error connecting to ${config.host} as ${config.user}\nError: ${error.message}`)
+        })
+} else {
+    console.error('no connection environment variable found');
+}
 
 module.exports = sequelize;
